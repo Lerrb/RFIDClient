272b7795c3dca13e4c62eba5fc382d22f8a3baef
RFIDServer <1018925913@qq.com>
2020-06-13 22:31:02 +0800

    LH

--- src/Socket/ObjectClient.java
+++ src/Socket/ObjectClient.java
@@ -7,9 +7,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 
-public class ClientTest {
+public class ObjectClient {
     public static void main(String[] args) throws IOException {
         Socket s = null;
         String ip = "127.0.0.1";
@@ -24,23 +23,10 @@ public class ClientTest {
             input = s.getInputStream ();
 
             objin = new ObjectInputStream (input);
-            Scanner sc=new Scanner(System.in);
-            while (true){
-                System.out.println("查询物流信息：0\n开始读卡：1");
-                System.out.print("请输入：");
-                int in=sc.nextInt();
-                if (in==0 || in==1){
-                  break;
-                }
-                System.out.println("输入不合法");
-            }
-
             int a = objin.readInt ();
             double b = objin.readDouble ();
-            //readObject不太行，建议使用Map集合传送数据
             StudentTest stu = (StudentTest) objin.readObject ();//readObject要转换你设定的对象
-            //System.out.println (a + ":" + b + ":" + stu.getName());
-            System.out.println(a+":"+b+":end");
+            System.out.println (a + ":" + b + ":" + stu.getName());
 
         } catch (Exception e) {
             e.printStackTrace ();
